1049a0244e4ae0a0e7924f1300be6392aec59156
BrauR <likitu264@gmail.com>
2023-01-30 13:55:23 -0600

    hola mundo
    -se agrego el script de hola mundo a la main camera
    -se usaron las funciones de Debug para imprimir mensajes

---
+++ Proy inicial EBAC/Assets/Scripts/Holamundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holamundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("algopaso");
+        Debug.Log("Algo pasaa");
+        Debug.LogWarning("Algo Salio Muy Mal");
+        Debug.LogError("Algo salio medianamente mal");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
